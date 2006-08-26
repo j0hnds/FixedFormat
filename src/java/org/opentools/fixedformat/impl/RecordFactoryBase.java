176481246df97f240708b28964ee4516cb68678d
djs <djs@a0480585-90f0-0310-8e05-92cc8036af5a>
2006-08-26 14:03:44 +0000

    New implementation
    git-svn-id: http://hagrid/svn-repos/Java/FixedFormat@1613 a0480585-90f0-0310-8e05-92cc8036af5a

---
+++ src/java/org/opentools/fixedformat/impl/RecordFactoryBase.java
@@ -0,0 +1,96 @@
+package org.opentools.fixedformat.impl;
+
+import java.util.Map;
+
+import org.opentools.fixedformat.Record;
+import org.opentools.fixedformat.RecordFactory;
+import org.opentools.fixedformat.RecordIdentifier;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+
+public class RecordFactoryBase implements RecordFactory, BeanFactoryAware
+{
+    private RecordIdentifier recordIdentifier;
+    private BeanFactory beanFactory;
+    private Map recordFormats;
+    private Map recordDataMap;
+    
+    public void setRecordIdentifier(RecordIdentifier identifier)
+    {
+        this.recordIdentifier = identifier;
+
+    }
+
+    public RecordIdentifier getRecordIdentifier()
+    {
+        return recordIdentifier;
+    }
+
+    public Object createRecord(String record)
+    {
+        if (recordIdentifier == null)
+        {
+            throw new IllegalStateException("No record identifier configured for this factory");
+        }
+        
+        String recordId = recordIdentifier.getRecordIdentifier(record);
+        if (recordId == null)
+        {
+            throw new RuntimeException("No record identifier found in record: " + record);
+        }
+        
+        if (recordFormats == null)
+        {
+            throw new IllegalStateException("No Record Format Map configured for this factory");
+        }
+        
+        Record recordFormat = (Record) recordFormats.get(recordId);
+        if (recordFormat == null)
+        {
+            throw new IllegalStateException("No record format configured for this record ID: " + recordId);
+        }
+        
+        if (recordDataMap == null)
+        {
+            throw new IllegalStateException("No record data map configured for this factory");
+        }
+        
+        Object recordData = recordDataMap.get(recordId);
+        if (recordData == null)
+        {
+            throw new IllegalStateException("No record data object configured for this record ID:" + recordId);
+        }
+        
+        recordFormat.scanRecord(record, recordData);
+        
+        return recordData;
+    }
+
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException
+    {
+        this.beanFactory = beanFactory;
+
+    }
+
+    public Map getRecordDataMap()
+    {
+        return recordDataMap;
+    }
+
+    public void setRecordDataMap(Map recordDataMap)
+    {
+        this.recordDataMap = recordDataMap;
+    }
+
+    public Map getRecordFormats()
+    {
+        return recordFormats;
+    }
+
+    public void setRecordFormats(Map recordFormatMap)
+    {
+        this.recordFormats = recordFormatMap;
+    }
+
+}
